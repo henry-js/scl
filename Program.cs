eb30d12d24ca2ddd68970c82e50251f9459f0c91
James Henry <jhenry@vigosoftware.com>
2023-06-11 15:01:19 +0100

    init commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.CommandLine;
+using System.Text;
+
+var fileOption = new Option<FileInfo?>(
+    "--file",
+    "The file to read and display on the console.");
+
+var rootCommand = new RootCommand("Sample app for System.CommandLine");
+rootCommand.AddOption(fileOption);
+
+rootCommand.SetHandler((file) => ReadFile(file!), fileOption);
+
+return await rootCommand.InvokeAsync(args);
+
+static void ReadFile(FileInfo file)
+{
+    File.ReadAllLines(file.FullName).ToList()
+        .ForEach(Console.WriteLine);
+}
